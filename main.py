dfc5e32f26c7dcef94659440af9376364dd0b5ea
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-13 16:46:28 +0200

    Update main.py: implemented logic

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ from mfrc522 import MFRC522
 TEAM_ID         = "code888"
 WIFI_SSID       = "EdNet"
 WIFI_PASSWORD   = "Huawei@123"
+
+MQTT_BROKER     = "157.173.101.159"
+MQTT_PORT       = 1883
+MQTT_CLIENT_ID  = b"esp8266_" + ubinascii.hexlify(machine.unique_id())
