1bbd8940cf3840a48f98203b8077ff422fd64503
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-13 16:46:27 +0200

    Update app.py: implemented logic

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 TEAM_ID = "code888"
 MQTT_BROKER = "157.173.101.159"
 TOPIC_STATUS = f"rfid/{TEAM_ID}/card/status"
+TOPIC_TOPUP = f"rfid/{TEAM_ID}/card/topup"
+TOPIC_BALANCE = f"rfid/{TEAM_ID}/card/balance"
+
+def on_connect(client, userdata, flags, rc, properties=None):
