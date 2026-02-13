fae55b03b4a71439ffe9f3f670b55d1ebd7e4f2e
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-13 16:46:28 +0200

    Update main.py: implemented logic

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ import machine
 from umqtt.simple import MQTTClient
 from mfrc522 import MFRC522
 
+# ===================== CONFIG - CHANGE THESE =====================
+TEAM_ID         = "code888"
+WIFI_SSID       = "EdNet"
+WIFI_PASSWORD   = "Huawei@123"
