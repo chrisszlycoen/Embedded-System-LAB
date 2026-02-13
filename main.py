32643605b68180b21d1ce8a87e47ce7f82da5898
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-13 16:46:28 +0200

    Update main.py: implemented logic

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ WIFI_PASSWORD   = "Huawei@123"
 MQTT_BROKER     = "157.173.101.159"
 MQTT_PORT       = 1883
 MQTT_CLIENT_ID  = b"esp8266_" + ubinascii.hexlify(machine.unique_id())
+
+# MQTT Topics
+BASE_TOPIC      = "rfid/{}/".format(TEAM_ID)
+STATUS_TOPIC    = BASE_TOPIC + "card/status"      
