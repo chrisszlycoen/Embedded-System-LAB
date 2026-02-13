b6bd86e311aa2c7ec418864df4e0ad42fb581779
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-13 16:46:27 +0200

    Update app.py: implemented logic

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO
 import paho.mqtt.client as mqtt
 import json
+
+app = Flask(__name__)
+
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
