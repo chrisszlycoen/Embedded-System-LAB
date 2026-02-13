1794dc6c96053ea4af9093ad1c2f2b4865846d44
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-13 16:46:27 +0200

    Update app.py: implemented logic

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ import json
 app = Flask(__name__)
 
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+
+TEAM_ID = "code888"
+MQTT_BROKER = "157.173.101.159"
+TOPIC_STATUS = f"rfid/{TEAM_ID}/card/status"
